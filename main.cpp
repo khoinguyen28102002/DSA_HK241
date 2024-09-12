ed4aaa2835c9202648e2ec131bdbc006eead0362
khoinguyen28102002 <khoinguyennguyen914@gmail.com>
2024-09-12 20:02:41 +0700

    first push

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include "list/listheader.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    xlistDemo1();
+    cout << "Assignment-1" << endl;
+    return 0;
+}
+
